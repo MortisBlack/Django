80b97919bb31de59b41e90a6525a51390323304a
MortisBlack <jesus.urrego.iuc@gmail.com>
2021-08-08 00:37:25 -0700

    login
    Hice todo lo que corresponde a login basico, asi como la pagina del usuario

--- django-website/hackershack_website/settings.py
+++ django-website/hackershack_website/settings.py
@@ -120,7 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_REDIRECT_URL = "index"
+LOGIN_URL = "login"
+LOGOUT_REDIRECT_URL = "index"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- django-website/hackershack_website/urls.py
+++ django-website/hackershack_website/urls.py
@@ -13,14 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from re import template
 from django.contrib import admin
+from django.contrib import auth
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
-    path('about', views.about, name="about"),
-    path('contact', views.contact, name="contact")
+    path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path('accounts/profile', views.ProfileView.as_view(), name="profile"),
+
+    # Django Auth: aqui se esta usando una aplicacion preinstalada -> "auth"
+                                                        # aqui agregamos este atributo para poder indicar exactamente a donde queremos ir
+    path('/accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path('/accounts/logout', auth_views.LogoutView.as_view(), name="logout"),
 ]
